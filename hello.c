2ee063fde5959db684bbcc65b1c845af6bdd1f81
karthickgautham4 <35916053+karthickgautham4@users.noreply.github.com>
2018-02-04 14:01:30 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a ,i=1;
+printf("how much times the word to be printed");
+scanf("%d",&a);
+while(i<=a)
+{
+printf("\nhello");
+i++;
+}
+return0;
+}
