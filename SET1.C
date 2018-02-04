2c463cf7e5e802ce9ad4b9fda35001d68ffb26bb
karthickgautham4 <35916053+karthickgautham4@users.noreply.github.com>
2018-02-04 14:28:26 +0530

    Create SET1.C

---
+++ SET1.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num,i=40,r=70;
+clrscr();
+printf("print odd number in a given range i to r:");
+for (num=i;num<=r;num++)
+{
+if(num%2==1)
+printf("%d",num);
+}
+getch();
+}
