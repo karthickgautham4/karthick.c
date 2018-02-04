e3d55b2d74cee99836a8b1f980794092a273ce8e
karthickgautham4 <35916053+karthickgautham4@users.noreply.github.com>
2018-02-04 09:46:16 +0530

    Create +or-.c

---
+++ +or-.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the Number");
+scanf("%d",&a);
+if(a>0)
+printf("The Number is positive");
+else(a<0)
+printf("The Number is Negative");
+else
+printf("The Number is zero");
+getch();
+}
