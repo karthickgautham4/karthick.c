bcaf0b986bea8cb0e63d5e94b44ebb8dc4b6f9c4
karthickgautham4 <35916053+karthickgautham4@users.noreply.github.com>
2018-02-04 14:07:49 +0530

    Create int.c

---
+++ int.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int count=0;
+long a;
+printf(" enter the integers");
+scanf("%ld",&a);
+while(a!=0)
+{
+a=a/10;
+count++;
+}
+printf("count the no of integers %ld",count);
+getch();
+}
