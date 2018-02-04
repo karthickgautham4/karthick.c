a3187fbc418b8f100d9d3668e3c5d83b75266852
karthickgautham4 <35916053+karthickgautham4@users.noreply.github.com>
2018-02-04 14:14:21 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<conio.h)
+#include<math.h>
+int main()
+{
+int a,b,c;
+printf("enter the number":);
+scanf("%d",&a");
+printf("how much power do you want":);
+scanf("%d",&b);
+c=pwo(a,b);
+printf("the power value is %d",c);
+return0;
+}
