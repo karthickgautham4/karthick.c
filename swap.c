cd0a1917002ef51a21c05ef7c0c958e1b4813067
karthickgautham4 <35916053+karthickgautham4@users.noreply.github.com>
2018-02-19 13:36:34 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int temp,n1,n2;
+    clrscr();
+    scanf("%d %d",&n1,&n2);
+    temp=n1;
+    n1=n2;
+    n2=temp;
+    printf("%d %d",n1,n2);
+    return 0;
+}
