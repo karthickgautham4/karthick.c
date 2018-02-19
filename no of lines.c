1748d1ee0a3ad9299c3a1232e451cd6dd0cb8cec
karthickgautham4 <35916053+karthickgautham4@users.noreply.github.com>
2018-02-19 13:13:53 +0530

    Create no of lines.c

---
+++ no of lines.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void) 
+{
+
+	char str[50];
+   
+  clrscr();
+
+	int i,count=1;
+
+	printf("enter the sentence:");
+
+	scanf("%[^\n]s",str);
+
+	for(i=0;str[i]!='\0';i++)
+
+	{
+
+		if(str[i]=='.')
+
+		{
+
+			count=count+1;
+
+		}
+
+	}
+
+	printf("\nno of lines is=%d",count);
+
+	return 0;
+
+}
