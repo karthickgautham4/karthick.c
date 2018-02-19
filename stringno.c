c7e1064a6eddc0706b12319dc304de5e79cb0de7
karthickgautham4 <35916053+karthickgautham4@users.noreply.github.com>
2018-02-19 13:59:54 +0530

    Create stringno.c

---
+++ stringno.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	char a[100];
+  clrscr();
+	int i,n;
+	scanf("%s",a);
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    printf("%s\n",a);
+	 }
+	 return 0;
+}
