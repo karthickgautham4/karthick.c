b74551eae36332dc916a1ab66e14c44216683f68
karthickgautham4 <35916053+karthickgautham4@users.noreply.github.com>
2018-03-09 15:54:38 +0530

    Create k78.c

---
+++ k78.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() 
+{
+    int n,i;
+    clrscr();
+    scanf("%d",&n);
+    if(n%13==0)
+    {
+        printf("yes...%d is a multiple of 13",n);
+    }
+    
+    else
+    
+    {
+    printf("no");
+    }
+	return 0;
+}
