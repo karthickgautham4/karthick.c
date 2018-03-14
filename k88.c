ebc4717d7e14eda51bff99dccbf65821fb002311
karthickgautham4 <35916053+karthickgautham4@users.noreply.github.com>
2018-03-14 16:14:00 +0530

    Update k88.c

--- k88.c
+++ k88.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include<conio.h>
 int main(void) 
 {
 int x,y,remainder,lcm,gcd,a,b;
@@ -11,13 +10,18 @@ scanf("%d",&y);
 a=x;
 b=y;
 do
+
 {
+
 remainder=x%y;
 if(remainder==0)
 break;
 x=y;
 y=remainder;
-}while(remainder!=0);
+
+}
+
+while(remainder!=0);
 gcd=y;
 lcm=(a*b)/gcd;
 printf("\n the lcm of the given number is: %d",lcm);
