31146e37bf1abba008477743ef25923b89ba2b6f
karthickgautham4 <35916053+karthickgautham4@users.noreply.github.com>
2018-03-19 16:03:33 +0530

    Update k91.c

--- k91.c
+++ k91.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include<conio.h>
-int main(void) 
+void main() 
 {
 	int l,b,h;
-  clrscr();
+        clrscr();
 	scanf("%d %d %d",&l,&b,&h);
 	int volume,tsa;
 	volume=l*b*h;
 	tsa=2*((l*b)+(b*h)+(h*l));
 	printf("\nVOLUME=%d",volume);
 	printf("\nTSA=%d",tsa);
-	return 0;
+	getch();
 }
