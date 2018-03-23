9d4207848d9c996946238fac7d306fdbd9b79335
karthickgautham4 <35916053+karthickgautham4@users.noreply.github.com>
2018-03-23 16:02:22 +0530

    Create k95.c

---
+++ k95.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include<conio.h>
+
+int main(void) 
+{
+	float p,r,result;
+	int si,n;
+	scanf("%f %d %f",&p,&n,&r);
+	result=((p*n*r)/100);
+	si=floor(result);
+	printf("\n%d",si);
+	return 0;
+  getch();
+}
