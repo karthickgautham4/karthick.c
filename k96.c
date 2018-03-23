c5f41219d0b8b801305cd3afbef57d2b710fabb3
karthickgautham4 <35916053+karthickgautham4@users.noreply.github.com>
2018-03-23 16:45:55 +0530

    Create k96.c

---
+++ k96.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a,i,count=1;
+printf("Enter the number");
+scanf("%d",&a);
+for(i=2;i<a;i++)
+{
+if(a%i==0)
+ count++;
+ }
+ if(count!=1)
+ printf("yes");
+ else
+ printf("No");
+}
