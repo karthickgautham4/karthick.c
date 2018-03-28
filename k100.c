aa51d0d12d26777bdf271ce27c3bddb370e2a702
karthickgautham4 <35916053+karthickgautham4@users.noreply.github.com>
2018-03-28 16:52:03 +0530

    Create k100.c

---
+++ k100.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,result=1,remainder=0;
+    scanf("%d",&n);
+    while(n!='\0')
+    
+    {
+        
+        
+        remainder=n%10;
+        
+        result=result*remainder;
+       
+       
+       n=n/10;
+    }
+    
+    printf("%d",result);
+    
+    
+    getch();
+
+}
